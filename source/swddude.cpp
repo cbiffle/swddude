fa084063f81bb0154a18a898943b59ff8ded5a8d
Cliff L. Biffle <code@cliffle.com>
2012-03-19 16:37:06 -0700

    Added heuristic enumeration of ARMv6M components.

--- source/swddude.cpp
+++ source/swddude.cpp
@@ -31,12 +31,15 @@
 #include "libs/log/log_default.h"
 #include "libs/command_line/command_line.h"
 
+#include <vector>
+
 #include <unistd.h>
 #include <stdio.h>
 #include <ftdi.h>
 
 using namespace Err;
 using namespace Log;
+using std::vector;
 
 enum PinData
 {
@@ -245,112 +248,194 @@ Error swd_reset(ftdi_context & ftdi)
     return success;
 }
 /******************************************************************************/
-Error crawl_memory_ap(DebugAccessPort &dap, uint8_t ap) {
-  uint32_t base;
-  Check(dap.post_read_ap_in_bank(2));
-  Check(dap.read_rdbuff(&base));
+Error read_cpuid(AccessPort &ap, uint32_t *cpuid) {
+  Check(ap.write(0x04, 0xE000ED00));  // CPUID register on ARMv6M
+  return ap.read_blocking(0x0C, cpuid, 100);
+}
+/******************************************************************************/
+Error crawl_unknown_peripheral(AccessPort &ap, uint32_t regfile);
+/******************************************************************************/
+Error crawl_rom_table(AccessPort &ap, uint32_t base_addr,
+                                      uint32_t regfile,
+                                      uint32_t size) {
+  debug(1, "Found ROM Table.");
 
-  if ((base & 3) == 3) {
-    // ARM ADIv5-style debug entry
-    uint32_t base_addr = base & ~0xFFF;
-    
-    // Set up the TAR/DRW registers to read the register space.
-    Check(dap.select_ap_bank(ap, 0));
-    Check(dap.write_ap_in_bank(0, 2));  // Set CSW to 4-byte transactions
-    for (uint32_t reg = 0; reg < 4; ++reg) {
-      Check(dap.write_ap_in_bank(1, base_addr + 0xFF0 + reg*4));  // TAR
-      Check(dap.post_read_ap_in_bank(3));
+  CheckEQ(size, 4096);
 
-      uint32_t contents;
-      Check(dap.read_rdbuff(&contents));
+  uint32_t memtype;
+  Check(ap.write(0x04, regfile + 0xFCC));  // MEMTYPE address into TAR.
+  Check(ap.read_blocking(0x0C, &memtype, 100));
 
-      debug(1, "  Component ID%d (%08X) = %08X", reg,
-          base_addr + 0xFF0 + reg*4, contents);
+  if (memtype & 1) {
+    debug(1, "ROM Table is on a common bus with system memory.");
+  } else {
+    debug(1, "ROM Table is on a dedicated bus.");
+  }
 
-      switch (reg) {
-        case 0:
-          CheckEQ(contents, 0x0D);
-          break;
+  vector<uint32_t> next_layer;
 
-        case 1: {
-          uint8_t component_class = (contents >> 4) & 0xF;
-          switch (component_class) {
-            case 0:
-              debug(1, "    Generic Verification Component");
-              break;
+  Check(ap.write(0x04, base_addr));
+  for (uint32_t i = 0; i < (0xF00 / 4); ++i) {
+    uint32_t entry;
+    Check(ap.read_blocking(0x0C, &entry, 100));
 
-            case 1:
-              debug(1, "    ROM Table");
-              break;
-
-            case 9:
-              debug(1, "    Debug Component");
-              break;
-
-            case 0xB:
-              debug(1, "    Peripheral Test Block");
-              break;
-
-            case 0xD:
-              debug(1, "    OptimoDE Data Engine Subsystem");
-              break;
-
-            case 0xE:
-              debug(1, "    Generic IP Component");
-              break;
-
-            case 0xF:
-              debug(1, "    PrimeCell Component");
-              break;
-          }
-          break;
-        } 
-
-        case 2:
-          CheckEQ(contents, 0x05);
-          break;
-
-        case 3:
-          CheckEQ(contents, 0xB1);
-          break;
+    if ((entry & (1 << 1)) == 0) {
+      // 8-bit entry; construct it from the three consecutive words.
+      entry <<= 24;
+      for (int i = 0; i < 3; ++i) {
+        uint32_t part;
+        Check(ap.read_blocking(0x0C, &part, 100));
+        entry = (entry >> 8) | (part << 24);
       }
     }
 
-    uint64_t peripheral_id = 0;
-
-    for (uint32_t reg = 0; reg < 4; ++reg) {
-      Check(dap.write_ap_in_bank(1, base_addr + 0xFE0 + reg*4));  // TAR
-      Check(dap.post_read_ap_in_bank(3));
-
-      uint32_t contents;
-      Check(dap.read_rdbuff(&contents));
-
-      peripheral_id |= (contents & 0xFF) << (reg * 8);
+    if (entry == 0) {
+      // End of ROM table!
+      break;
     }
 
-    for (uint32_t reg = 4; reg < 8; ++reg) {
-      Check(dap.write_ap_in_bank(1, base_addr + 0xFC0 + reg*4));  // TAR
-      Check(dap.post_read_ap_in_bank(3));
-
-      uint32_t contents;
-      Check(dap.read_rdbuff(&contents));
-
-      peripheral_id |= (contents & 0xFF) << (reg * 8);
+    if ((entry & (1 << 0)) == 0) {
+      // Entry not present; skip it.
+      debug(1, "[%d]: not present", i);
+      continue;
     }
 
-    debug(1, "  Peripheral ID = %016llX", peripheral_id);
-    debug(1, "    Part No:     %llX", peripheral_id & 0xFFF);
-    if (peripheral_id & (1 << 19)) {
-      debug(1, "    JEP106 ID:   %llX", (peripheral_id >> 12) & 0x7F);
-      debug(1, "    JEP106 CC:   %llX", (peripheral_id >> 32) & 0xF);
-    }
-    debug(1, "    Rev:         %llX", (peripheral_id >> 20) & 0xF);
-    debug(1, "    Cust.Mod:    %llX", (peripheral_id >> 24) & 0xF);
-    debug(1, "    Manuf.Rev:   %llX", (peripheral_id >> 28) & 0xF);
-    debug(1, "    4KiB Blocks: 2^%lld", (peripheral_id >> 36) & 0xF);
+    int32_t offset = entry & ~0xFFF;
+    uint32_t sub_regfile = base_addr + offset;
+    next_layer.push_back(sub_regfile);
+    debug(1, "[%d]: base + %08X = %08X", i, offset, sub_regfile);
+  }
+
+  for (vector<uint32_t>::iterator it = next_layer.begin();
+       it != next_layer.end();
+       ++it) {
+    crawl_unknown_peripheral(ap, *it);
   }
 
   return success;
+}
+/******************************************************************************/
+Error crawl_armv6m_dwt(AccessPort &ap, uint32_t base_addr, 
+                                       uint32_t regfile,
+                                       uint32_t size) {
+  debug(1, "Found ARMv6M DWT");
+  return success;
+}
+/******************************************************************************/
+Error crawl_armv6m_bpu(AccessPort &ap, uint32_t base_addr, 
+                                       uint32_t regfile,
+                                       uint32_t size) {
+  debug(1, "Found ARMv6M BPU");
+  return success;
+}
+/******************************************************************************/
+Error crawl_armv6m_scs(AccessPort &ap, uint32_t base_addr, 
+                                       uint32_t regfile,
+                                       uint32_t size) {
+  debug(1, "Requesting halt...");
+  ap.write(0x04, 0xE000EDF0);
+  ap.write(0x0C, (0xA05F << 16)
+               | (1 << 1)  // Request a halt
+               | (1 << 0)  // Enable debugging
+               );
+  
+  for (int i = 0; i < 10; ++i) {
+    ap.write(0x04, 0xE000EDF0);
+    uint32_t dhcsr;
+    ap.read_blocking(0x0C, &dhcsr, 100);
+    if (dhcsr & (1 << 17)) {
+      debug(1, "Halted!");
+      break;
+    } else {
+      debug(1, "Waiting for halt...");
+      usleep(1000000);
+    }
+  }
+
+  return success;
+}
+/******************************************************************************/
+Error crawl_unknown_peripheral(AccessPort &ap, uint32_t regfile) {
+  debug(1, "--- Peripheral at %08X in AP %02X", regfile, ap.index());
+
+  // Load Component ID registers!
+  uint32_t component_id[4];
+  Check(ap.write(0x04, regfile + 0xFF0));  // First address into TAR
+  Check(ap.read_blocking(0x0C, &component_id[0], 100));
+  Check(ap.read_blocking(0x0C, &component_id[1], 100));
+  Check(ap.read_blocking(0x0C, &component_id[2], 100));
+  Check(ap.read_blocking(0x0C, &component_id[3], 100));
+
+  for (int i = 0; i < 4; ++i) {
+    debug(1, "Component ID %d = %08X", i, component_id[i]);
+  }
+
+  // ADIv5 places certain constraints on component IDs.  Sanity check them.
+  CheckEQ(component_id[0], 0x0D);
+  CheckEQ(component_id[2], 0x05);
+  CheckEQ(component_id[3], 0xB1);
+
+  // Load Peripheral ID4, which tells us how large this component truly is.
+  Check(ap.write(0x04, regfile + 0xFD0));
+  uint32_t peripheral_id4;
+  Check(ap.read_blocking(0x0C, &peripheral_id4, 100));
+
+  uint32_t log2_size_in_blocks = (peripheral_id4 >> 4) & 0xF;
+  uint32_t size = (1 << log2_size_in_blocks) * 4 * 1024;
+  debug(1, " Size = 2^%u blocks = %u bytes", log2_size_in_blocks, size);
+
+  // regfile is the address of the *last* block -- compute the *first* block.
+  uint32_t base_addr = (regfile + 4 * 1024) - size;
+
+  // Now, dispatch on the type of this component.
+  uint8_t component_class = (component_id[1] >> 4) & 0xF;
+  switch (component_class) {
+    case 1:
+      return crawl_rom_table(ap, base_addr, regfile, size);
+  }
+
+  // Hm, Cortex-M0 seems to return bogus component classes.
+  // Attempt to recognize processor heuristically.
+  uint32_t cpuid;
+  Check(read_cpuid(ap, &cpuid));
+  debug(1, "CPUID = %08X", cpuid);
+  if (((cpuid >> 16) & 0xF) == 0xC) {  // ARMv6M
+    switch (regfile) {
+      case 0xE000E000:
+        return crawl_armv6m_scs(ap, base_addr, regfile, size);
+
+      case 0xE0001000:
+        return crawl_armv6m_dwt(ap, base_addr, regfile, size);
+
+      case 0xE0002000:
+        return crawl_armv6m_bpu(ap, base_addr, regfile, size);
+    }
+  }
+
+  debug(1, "Unknown component class %X.", component_class);
+  return success;
+}
+/******************************************************************************/
+Error crawl_memory_ap(AccessPort &ap) {
+  uint32_t base;
+  Check(ap.read_blocking(0xF8, &base, 10));
+
+  if ((base & 3) != 3) {
+    // This is a "legacy" device not compliant with ARM ADIv5.
+    debug(1, "Encountered non-ADIv5 legacy device");
+    return success;
+  }
+
+  // The MEM-AP may be in an arbitrary state.  Fix that.
+  Check(ap.write(0x00, (1 << 4)     // Auto increment.
+                     | (2 << 0)));  // Transfer size 2^2 = 4 bytes.
+
+  // Extract the Debug Register File address from the BASE register.
+  // This points to the last 4KiB block of this debug device.
+  uint32_t regfile = base & ~0xFFF;
+  debug(1, "register file at %08X", regfile);
+
+  return crawl_unknown_peripheral(ap, regfile);
 }
 /******************************************************************************/
 Error enumerate_access_ports(ftdi_context &ftdi) {
@@ -360,18 +445,21 @@ Error enumerate_access_ports(ftdi_context &ftdi) {
   DebugAccessPort dap(&swd);
   Check(dap.reset_state());
 
-  for (uint32_t ap = 0; ap < 256; ++ap) {
-    Check(dap.select_ap_bank(ap, 0xF));  // Last bank contains ident info
+  for (uint32_t i = 0; i < 256; ++i) {
+    AccessPort ap(&dap, i);
     
+    Check(ap.post_read(0xFC));
     uint32_t idr;
-    Check(dap.post_read_ap_in_bank(3));
-    Check(dap.read_rdbuff(&idr));
+    Check(ap.read_last_result(&idr));
+    
     if (idr == 0) continue;  // AP not implemented
 
-    debug(1, "AP %02X IDR = %08X", ap, idr);
+    debug(1, "AP %02X IDR = %08X", i, idr);
 
     if (idr & (1 << 16)) {  // Memory Access Port
-      Check(crawl_memory_ap(dap, ap));
+      Check(crawl_memory_ap(ap));
+    } else {
+      debug(1, "Unknown AP type.");
     }
   }
 
